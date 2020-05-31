25859f5c4ae91342b2b5907682b80ff320d74c24
Joaquin Torres <mod4rchive@gmail.com>
2020-05-31 16:56:02 -0400

    TDA actualizado para incluir rama

--- main.rkt
+++ main.rkt
@@ -1,34 +1,39 @@
 #lang racket
 
-;zones = (history index localRepo remoteRepo)
-;commit = (mensaje (archivo1 ... archivoN))
-;history: log de las funciones aplicadas de la forma '( '(tiempo1 funcion1) ... '(tiempoN funcionN) )
+;zones = ((history index localRepo remoteRepo branch1) ... (... branchN))
+;commit = (archivo1 ... archivoN)
+;localRepo = remoteRepo = (commit1 ... commitN)
+;history: log de las funciones aplicadas de la forma '( '(tiempoN funcionN) ... '(tiempo1 funcion1) )
 ;en orden cronologico
 (define zonas1
-  (list null null null null))
+  (list (list (list "HISTORY") (list "INDEX") (list "LOCAL REPO") (list "REMOTE REPO") "master")))
 
 (define get-history
   ( lambda (zones-list)
-     (car zones-list)))
+     (car (car zones-list))))
 
 (define get-index
   ( lambda (zones-list)
-     (cadr zones-list)))
+     (cadr (car zones-list))))
 
 (define get-localRepo
   ( lambda (zones-list)
-     (caddr zones-list)))
+     (caddr (car zones-list))))
 
 (define get-remoteRepo
   ( lambda (zones-list)
-     (cadddr zones-list)))
+     (cadddr (car zones-list))))
+
+(define get-currentBranch
+  ( lambda (zones-list)
+     (car (cddddr (car zones-list)))))
 
 
 ;crea la unidad de historial. recibe el nombre de la funcion como string. se arreglara ingreso de
 ;datos despues.
 (define create-log
   ( lambda (name-of-function-string)
-     (list (current-seconds) name-of-function-string)))
+     (list (number->string (current-seconds)) name-of-function-string)))
 
 
 ;actualiza el historial con una funcion dada. recibe una lista de log y el historial a sumar y retorna
@@ -40,7 +45,7 @@
 
 ;añade el historial actualizado al TDA zonas de la funcion realizada. recibe el historial actualizado
 ;(lista), el tda zonas y retorna el tda zonas con el historial actualizado.
-(define add-history
+(define set-history
   ( lambda (updated-history zones)
      (append (list updated-history) (cdr zones))))
 
@@ -51,27 +56,34 @@
 
 (define pull
   (lambda (zones)
-               (add-history (update-history (create-log "pull") zones) zones)))
+               (set-history (update-history (create-log "pull") zones) zones)))
 
 (define push
   (lambda (zones)
-               (add-history (update-history (create-log "push") zones) zones)))
+               (set-history (update-history (create-log "push") zones) zones)))
 
 (define commit
   (lambda (message)
     (lambda (zones)
-      (add-history (update-history (create-log (string-append "commit -m " message)) zones) zones))))
+      (set-history (update-history (create-log (string-append "commit -m " message)) zones) zones))))
 
 (define file-list-to-string
   (lambda (file-list)
-    (apply string-append (map (lambda (file-string) (string-append " " file-string)) file-list))))
+    (if null?
+        ""
+        (apply string-append (map (lambda (file-string) (string-append " " file-string)) file-list)))))
 
 (define add
   (lambda (file-list)
     (lambda (zones)
-      (add-history
+      (set-history
        (update-history
         (create-log
          (string-append "add" (file-list-to-string file-list)))
         zones)
        zones))))
+
+
+;(define zonas->string
+ ; (lambda (zones)
+  ;  ()))
